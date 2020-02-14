a7bd48c5877a8d91a012982950238844082ef3eb
Steve Richey <srichey@gowithfloat.com>
2020-02-14 11:41:12 -0600

    replace placeholder test

--- tests/test_command_module.py
+++ tests/test_command_module.py
@@ -7,10 +7,13 @@ Tests for command_module.py
 
 import unittest
 
+from clippy.command_module import CommandModule
+
 
 class TestCommandModule(unittest.TestCase):
-    def test_(self):
-        self.assertTrue(True)
+    def test_create(self):
+        command_module = CommandModule(index=0)
+        self.assertIsNotNone(command_module)
 
 
 if __name__ == "__main__":
